31a50dfb12db041fdf1d7ae952003b80041fbf6d
Serena Leung <serenaleung1@gmail.com>
2017-05-22 19:14:43 -0700

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Serena", email: "s@e.com", password: "123", password_confirmation: "123")
+User.create(name: "Jason", email: "j@t.com", password: "123", password_confirmation: "123")
+User.create(name: "Chelsea", email: "c@t.com", password: "123", password_confirmation: "123")
+
+
+Messages.create(details: "Party", amount: 30, user_id: 1)
+Splitters.create(user_id: 2, amountOwing: 15, message_id: 1)
+Splitters.create(user_id: 3, amountOwing: 15, message_id: 1)
+
+User.where(api_token: nil).each {|u| u.update(api_token: SecureRandom.urlsafe_base64(32)) }
